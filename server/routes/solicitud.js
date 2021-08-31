0bd9520ac8ffe9d7c6c5d830dc04637253601ec0
christian pillaga s <christian_pillaga_s@hotmail.com>
2021-08-31 15:26:12 -0500

    notificacion push

--- server/routes/solicitud.js
+++ server/routes/solicitud.js
@@ -129,7 +129,7 @@ app.put('/solicitud/:id', function(req, res) {
                 mensaje = mensaje + solicitud.estado + " por " + solicitud.comentario;
             }
 
-            fcm.userNotification(usuarioDB.fcm, `Respuesta a Solicitud`, mensaje, { sol: solicitudDB });
+            fcm.userNotification(usuarioDB.fcm, `Respuesta a Solicitud`, mensaje, { id: solicitudDB._id+"" });
 
             res.json({
                 ok: true,
